1c2eae730809882cba1c9ec6ba47fc2ce63b0bef
Martin <166133291+MH020@users.noreply.github.com>
2025-10-27 11:36:42 +0100

    selvte runs now

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+    server: {
+    port: 8080,  
+    host: true,    
+    strictPort: true 
+  }
 })
